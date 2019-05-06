76e74503ebd67c6c09c9b4239a694a4b363b5867
semkaway <semkaway@gmail.com>
2019-05-06 16:28:09 +0300

    [ADD] general functionality to each button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,46 @@
 
 let start = document.getElementById('buttonStart')
-console.log("start: ", start)
-start.addEventListener('click', function() {
-    console.log("Start!!")
-})
+let pause = document.getElementById('buttonPause')
+let lap = document.getElementById('buttonLap')
+let reset = document.getElementById('buttonReset')
+let seconds = 0
+let running = false
+let timer
+
+function addSeconds() {
+    console.log("running", running)
+    // if (running) {
+    //     setInterval(function(){seconds++}, 100)
+    // }
+    console.log(seconds++)
+}
+
+function startCount() {
+    if (!running) {
+        console.log("Start!!")
+        running = true
+        timer = setInterval(addSeconds, 1000)
+    }
+}
+
+function pauseCount() {
+    console.log("Pause!!")
+    running = false
+    clearInterval(timer)
+}
+
+function addLap() {
+    console.log("Lap!!")
+}
+
+function resetCount() {
+    console.log("Stop!!")
+    running = false
+    seconds = 0
+    clearInterval(timer)
+}
+
+start.addEventListener('click', startCount)
+pause.addEventListener('click', pauseCount)
+lap.addEventListener('click', addLap)
+reset.addEventListener('click', resetCount)
