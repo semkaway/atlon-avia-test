83a5dd351ad73777a0d7f0d8e451f088209fc2fb
semkaway <semkaway@gmail.com>
2019-05-06 17:30:04 +0300

    [ADD] start, pause, stop functionality

--- js/script.js
+++ js/script.js
@@ -3,23 +3,54 @@ let start = document.getElementById('buttonStart')
 let pause = document.getElementById('buttonPause')
 let lap = document.getElementById('buttonLap')
 let reset = document.getElementById('buttonReset')
-let seconds = 0
+let displayTime = document.getElementById('displayTime')
 let running = false
+let time = 0
+let offset
 let timer
 
-function addSeconds() {
-    console.log("running", running)
-    // if (running) {
-    //     setInterval(function(){seconds++}, 100)
-    // }
-    console.log(seconds++)
+// console.log("displayTime: ", displayTime.innerHTML)
+
+function updateTime() {
+    time += timePassed()
+    let formatTime = formattedTime(time)
+    displayTime.textContent = formatTime
+}
+
+function timePassed() {
+    let now = Date.now()
+    let difference = now - offset
+    offset = now
+    return difference
+}
+
+function formattedTime(inputTime) {
+    let newTime = new Date(inputTime)
+    let minutes = newTime.getMinutes().toString()
+    let seconds = newTime.getSeconds().toString()
+    let milliseconds = newTime.getMilliseconds().toString()
+
+    if (minutes.length < 2) {
+        minutes = "0" + minutes
+    }
+
+    if (seconds.length < 2) {
+        seconds = "0" + seconds
+    }
+
+    while (milliseconds.length < 3) {
+        milliseconds = "0" + milliseconds
+    }
+
+    return `${minutes}:${seconds}:${milliseconds}`
 }
 
 function startCount() {
     if (!running) {
         console.log("Start!!")
+        offset = Date.now()
         running = true
-        timer = setInterval(addSeconds, 1000)
+        timer = setInterval(updateTime, 10)
     }
 }
 
@@ -36,7 +67,7 @@ function addLap() {
 function resetCount() {
     console.log("Stop!!")
     running = false
-    seconds = 0
+    time = 0
     clearInterval(timer)
 }
 
